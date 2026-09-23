26af9577105b3b2f4c1f97de31a0860c92452ebd
den.kyaan <steegmans.kyani@icloud.com>
2026-09-23 23:50:56 +0200

    Refresh cached translated offline pages

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='uptilldawn-public-v6'
+const CACHE='uptilldawn-public-v7'
 const PUBLIC_ASSETS=['/offline.html','/offline-public.html','/up-till-dawn-mark.webp']
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(PUBLIC_ASSETS))))
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('uptilldawn-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())))

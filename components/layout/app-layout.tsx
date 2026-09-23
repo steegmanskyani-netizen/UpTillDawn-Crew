e46486a8794405be70ae63d968ee113123561acf
den.kyaan <steegmans.kyani@icloud.com>
2026-09-23 18:58:21 +0200

    Hide urgent control from print output

--- components/layout/app-layout.tsx
+++ components/layout/app-layout.tsx
@@ -21,7 +21,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
         </div>
         <div className="print:hidden"><MobileBottomNav /></div>
       </div>
-      <Link href="/incidents" className="fixed bottom-20 left-4 z-50 rounded-full bg-red-600 px-5 py-4 font-black text-white">URGENT</Link>
+      <Link href="/incidents" className="fixed bottom-20 left-4 z-50 rounded-full print:hidden bg-red-600 px-5 py-4 font-black text-white">URGENT</Link>
     </div>
   )
 }
